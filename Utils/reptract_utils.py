9ec099a4001cb378b2b2c0431c5d9b941e927172
valelorenzi9 <ruotasenza@gmail.com>
2022-02-07 08:33:41 +0000

    Adding bonferroni and FDR functions

--- Utils/reptract_utils.py
+++ Utils/reptract_utils.py
@@ -68,3 +68,43 @@ def per_gene_analysis(adata):
     sc.pl.pca_variance_ratio(bdata, log=True)
 
     return bdata
+
+# Benjamini-Hochberg and Bonferroni FDR helper functions.
+def bh(pvalues):
+    """
+    Computes the Benjamini-Hochberg FDR correction.
+
+    Input:
+        * pvals - vector of p-values to correct
+    """
+    pvalues = np.array(pvalues)
+    n = int(pvalues.shape[0])
+    new_pvalues = np.empty(n)
+    values = [ (pvalue, i) for i, pvalue in enumerate(pvalues) ]
+    values.sort()
+    values.reverse()
+    new_values = []
+    for i, vals in enumerate(values):
+        rank = n - i
+        pvalue, index = vals
+        new_values.append((n/rank) * pvalue)
+    for i in range(0, int(n)-1):
+        if new_values[i] < new_values[i+1]:
+            new_values[i+1] = new_values[i]
+    for i, vals in enumerate(values):
+        pvalue, index = vals
+        new_pvalues[index] = new_values[i]
+    return new_pvalues
+
+def bonf(pvalues):
+    """
+    Computes the Bonferroni FDR correction.
+
+    Input:
+        * pvals - vector of p-values to correct
+    """
+    new_pvalues = np.array(pvalues) * len(pvalues)
+    new_pvalues[new_pvalues>1] = 1
+    return new_pvalues
+
+
