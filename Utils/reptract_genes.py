e7446e8b059cb9a78f3f1afd98b1781adbbe4a9e
valelorenzi9 <ruotasenza@gmail.com>
2022-02-16 15:05:31 +0000

    Add list of marker genes

---
+++ Utils/reptract_genes.py
@@ -0,0 +1,12 @@
+markers = {
+'epithelial' : ['UPK3B', 'GATA2', 'GATA4', 'GATA3', 'WNT9B', 'LHX9', 'EPCAM', 'PAX8', 'PAX2', 'EMX2', 'LHX1', 'WNT7A', 'FZD10', 'LGR5', 
+'FOXJ1', 'OVGP1', 'SOX9', 'KLK11', 'KRT19', 'KRT7', 'KRT8', 'KRT18', 'KRT14', 'TP63', 'HNF1B', 'DACH1', 'DACH2', 'CDH1', 'RARA', 'RARB'], 
+'mesenchymal' : ['VIM', 'DCN', 'COL1A1', 'HOXA9', 'HOXA10', 'HOXA11', 'HOXA13', 'ACTA2', 'DES', 'WNT4', 'TSPAN8', 'LGR5', 'WNT5A', 'CTNNB1', 'MMP2'],
+'other' : ['PTPRC', 'CD68', 'CDH5', 'CCL21', 'LYVE1', 'PROX1', 'PLP1', 'MPZ', 'HBA1', 'ALAS2', 'NPHS1', 'MGP', 'MCAM', 'RERGL'],
+'male_specific' : ['SRY', 'RPS4Y1', 'DDX3Y'],
+'hormone_receptors' : ['AR', 'ESR1', 'ESR2', 'AMHR2', 'PGR', 'AVCR2', 'BMPR1A', 'SMAD1', 'SMAD5', 'SMAD8'],
+'EMT' : ['ZEB1', 'ZEB2', 'TWIST1', 'TWIST2', 'SNAI1', 'SNAI2'],
+'stress' : ['FOSB', 'FOS', 'JUN', 'ATF3', 'EGR1', 'HSPA1A', 'HSPA1B'],
+'growth_factors' : ['IGF1', 'IGF1R', 'TGFB1', 'TGFB2', 'EGF', 'EGFR', 'FGF1', 'FGF2', 'GH1', 'GHR']
+}
+
