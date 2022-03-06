e2a21f6fb3783d60249652fd59ea356e58ac554b
valelorenzi9 <ruotasenza@gmail.com>
2022-03-06 18:23:34 +0000

    Fix munge stats script

--- LDSC_CTS/LDSC_CTS_step2_mungestats.sh
+++ LDSC_CTS/LDSC_CTS_step2_mungestats.sh
@@ -4,36 +4,27 @@
 # Convert GWAS summary statistics file to .sumstats format #
 ############################################################
 
-# Assign script argument to variable 
-while getopts g:o: flag
+while getopts w:g: flag
 do
     case "${flag}" in
+        w) workdir=${OPTARG};;
         g) gwas=${OPTARG};;
-        o) outdir=${OPTARG};;
     esac
 done
-echo "GWAS name: $gwas";
-echo "Outdir: $outdir";
+echo -e "Workdir: $workdir\n";
+echo -e "GWAS of interest: $gwas\n"
 
-# Download SNPs that are listed in the HapMap3 project, if they haven't been downloaded already 
-
-if [ ! -f $outdir/w_hm3.snplist.bz2]
-then
-    echo "List of SNPs in HapMap3 project not found in specified outdir, it will be downloaded"
-    wget https://data.broadinstitute.org/alkesgroup/LDSCORE/w_hm3.snplist.bz2
-    bunzip2 w_hm3.snplist.bz2
-else
-    echo "List of SNPs in HapMap3 project found in specified outdir, it will NOT be re-downloaded"
-fi
+filename=$gwas
+name="${filename%%.*}"
 
 # Activate ldsc virtual environment 
-set -e 
-source /home/jovyan/my-conda-envs/ldsc/bin/activate 
+#set -e 
+#source /home/jovyan/my-conda-envs/ldsc/bin/activate 
 
 # Convert GWAS summary statistics to .sumstats format by using munge_sumstats.py script and keeping only SNPs that are listed in the HapMap3 project
 # ! Though not mentioned in the tutorial, it's important to reduce the chunk size otherwise it takes ages to run: by default chunksize = 5000000
 python /home/jovyan/ldsc/munge_sumstats.py \
---sumstats $outdir/$gwas.sumstats.gz \
---merge-alleles $outdir/w_hm3.snplist \
---out $gwas
+--sumstats $workdir/$gwas \
+--merge-alleles $workdir/w_hm3.snplist \
+--out $name \
 --chunksize 500000
